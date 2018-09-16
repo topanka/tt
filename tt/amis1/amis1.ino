0f112eca461a9967635fcaf0bb3b5483a338ce60
topanka <33791367+topanka@users.noreply.github.com>
2018-09-16 18:42:59 +0200

    Add files via upload

---
+++ tt/amis1/amis1.ino
@@ -0,0 +1,62 @@
+/* This example shows how to use the AMIS-30543 stepper motor
+driver with the AccelStepper library.
+
+You will need to install the AccelStepper library for this
+example to work.  Documentation and installation instructions for
+the AccelStepper library are available here:
+
+    http://www.airspayce.com/mikem/arduino/AccelStepper/
+
+Before using this example, be sure to change the
+setCurrentMilliamps line to have an appropriate current limit for
+your system.  Also, see this library's documentation for
+information about how to connect the driver:
+
+    http://pololu.github.io/amis-30543-arduino/
+*/
+
+#include <SPI.h>
+#include <AMIS30543.h>
+#include <AccelStepper.h>
+
+const uint8_t amisDirPin = 3;
+const uint8_t amisStepPin = 2;
+const uint8_t amisSlaveSelect = 10;
+
+AMIS30543 stepper;
+AccelStepper accelStepper(AccelStepper::DRIVER, amisStepPin, amisDirPin);
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode (amisSlaveSelect,OUTPUT);  
+  SPI.begin();
+  stepper.init(amisSlaveSelect);
+  delay(1);
+
+  stepper.resetSettings();
+  stepper.setCurrentMilliamps(500);
+//  stepper.setStepMode(32);
+  stepper.setStepMode(16);
+  stepper.enableDriver();
+
+  accelStepper.setMaxSpeed(2000.0);
+  accelStepper.setAcceleration(500.0);
+}
+
+void loop()
+{
+  uint16_t pos;
+  
+  pos=stepper.readPosition();
+  Serial.println(pos);
+  
+  accelStepper.runToNewPosition(0);
+  delay(500);
+  pos=stepper.readPosition();
+  Serial.println(pos);
+  
+  accelStepper.runToNewPosition(10000);
+  delay(500);
+}
+
---
+++ tt/teestep1/teestep1.ino
@@ -0,0 +1,107 @@
+/*==========================================================================
+ * This is a minimal sketch showing the usage of TeensyStep
+ *  
+ * STEP Pulses on Pin 2    (can be any pin)
+ * DIR  Signall on Pin 3   (can be any pin)
+ * 
+ * The target position is set to 1000 steps relative to the
+ * current position. The move command of the controller 
+ * moves the motor to the target position.  
+ *  
+ * Default parameters are 
+ * Speed:          800 steps/s
+ * Acceleration:  2500 steps/s^2
+ * 
+ * (slow, but good to start with since they will work on any normal stepper)
+ *
+ ===========================================================================*/
+
+#include <SPI.h>
+#include <AMIS30543.h>
+#include <StepControl.h>
+
+
+const uint8_t m1_dirPin = 3;
+const uint8_t m1_stepPin = 2;
+const uint8_t m1_ssPin = 9;
+
+const uint8_t m2_dirPin = 5;
+const uint8_t m2_stepPin = 4;
+const uint8_t m2_ssPin = 10;
+
+AMIS30543 m1_amis,m2_amis;
+Stepper m1_stepper(m1_stepPin,m1_dirPin);         // STEP pin: 2, DIR pin: 3
+Stepper m2_stepper(m2_stepPin,m2_dirPin);         // STEP pin: 2, DIR pin: 3
+StepControl<> controller;    // Use default settings 
+
+void setup()
+{
+  Serial.begin(115200);
+  
+  SPI.begin();
+
+  
+  m1_amis.init(m1_ssPin);
+  delay(1);
+  m1_amis.resetSettings();
+  m1_amis.setCurrentMilliamps(1000);
+  m1_amis.setStepMode(128);
+  m1_amis.enableDriver();
+  
+  m2_amis.init(m2_ssPin);
+  delay(1);
+  m2_amis.resetSettings();
+  m2_amis.setCurrentMilliamps(1000);
+  m2_amis.setStepMode(128);
+  m2_amis.enableDriver();
+  
+
+  m1_stepper.setMaxSpeed(30000);             // stp/s
+  m2_stepper.setMaxSpeed(15000);             // stp/s
+  
+  m1_stepper.setAcceleration(50000);    // stp/s^2
+  m2_stepper.setAcceleration(50000);    // stp/s^2
+  
+}
+
+void loop() 
+{
+  uint16_t pos1,pos2;
+  static uint16_t l_idx=0;
+
+  m1_stepper.setTargetRel(12800);  // Set target position to 1000 steps from current position
+  m2_stepper.setTargetRel(12800);  // Set target position to 1000 steps from current position
+/*
+  if(l_idx%2 == 0) {
+    m1_stepper.setMaxSpeed(500);             // stp/s
+    m1_stepper.setTargetRel(3200);  // Set target position to 1000 steps from current position
+    m2_stepper.setMaxSpeed(3000);             // stp/s
+    m2_stepper.setTargetRel(1600);  // Set target position to 1000 steps from current position
+  } else {
+    m1_stepper.setMaxSpeed(-1000);             // stp/s
+    m1_stepper.setTargetRel(3200);  // Set target position to 1000 steps from current position
+  }
+*/  
+  l_idx++;
+  
+//  controller.move(m2_stepper);    // Do the move
+//  delay(10);
+  controller.move(m1_stepper,m2_stepper);    // Do the move
+//  delay(10);
+  
+  pos1=m1_amis.readPosition();
+  pos2=m2_amis.readPosition();
+  Serial.print(pos1);
+  Serial.print(" ");
+  Serial.println(pos2);
+
+/*  
+  pos1=m1_stepper.getPosition();
+  pos2=m2_stepper.getPosition();
+  Serial.print(pos1);
+  Serial.print("*");
+  Serial.println(pos2);
+*/  
+
+}
+
